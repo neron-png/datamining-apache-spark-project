6efb8f71f9d299ac8fee5f1419b2ab589142a1e2
Neron-png <neronpanagiotopoulos@gmail.com>
2023-12-31 20:25:34 +0200

    Added outlier detection

--- main.py
+++ main.py
@@ -3,30 +3,33 @@ from pyspark.sql.dataframe import DataFrame as DF
 from pyspark import SparkContext as sc
 from pyspark.sql import SparkSession
 from kmeans import kmeansCluster, reduceClusters, single_link
+from outlierDetect import findOutliers
 
 if __name__ == "__main__":
     
     spark: SparkSession = SparkSession.builder.appName("dataminingProject").master("local[*]").getOrCreate()
     df: DF = spark.read.csv("./data-example2223.csv", header=False)
     
+    # PART 2
     clean_df = cleanup(df)
+    
+    # PART 3
     normalized_df = normalize(df)
-
+    
+    # PART 4
     clustered = kmeansCluster(normalized_df, n=150)
     clustered.show()
 
     # Remember to install pandas and matplotlib
-    # plot(clustered, "cluster")
+    plot(clustered, "cluster")
     
+    # Reduce clusters to 5
     reduceClusters = single_link(clustered, n=5)
     plot(reduceClusters, "realCluster")
     
-    # reduced_Kmeans = kmeansCluster(clustered.groupBy("cluster").avg("_c0", "_c1").withColumnRenamed("avg(_c0)", "_c0").withColumnRenamed("avg(_c1)", "_c1").drop("cluster"), n=25)
-    # reduced_Kmeans.show()
-    # plot(reduced_Kmeans)
     
-    # WIP: Reduce clusters to 5
-    # reducedClusters = single_link(clustered, n=5)
-    # # reducedClusters.show()
+    # PART 5
+    outlierDF = findOutliers(clustered, n_stdev=5)
+    outlierDF.show()
+    plot(outlierDF, "outlier")
     
-    # testing_listPlot(reducedClusters)
